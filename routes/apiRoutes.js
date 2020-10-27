6bd22f8511fe5cc8776f2784f6328fd182e90803
Max Gerringer <max.gerringer@gmail.com>
2020-10-27 15:46:21 -0400

    establish api routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,35 @@
+const Workout = require("../models/workout");
+const router = require("express").Router();
+
+router.get("/api/workouts", (req, res) => {
+  Workout.find({})
+  .then(data => {
+    res.json(data);
+  }).catch(err => {
+    console.log(err);
+    res.status(404).res.json(err);
+  });
+});
+
+router.post("/api/workouts", (req, res) => {
+  Workout.create({})
+  .then(data => {
+    res.json(data);
+  }).catch(err => {
+    console.log(err);
+    res.status(404).res.json(err);
+  });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+  Workout.findByIdAndUpdate(req.params.id, 
+    { $push: { exercises: req.body }},
+    { new: true})
+    .then(data => {
+      res.json(data);
+    }).catch(err => {
+      console.log(err);
+      res.status(404).res.json(err);
+    });
+});
+
